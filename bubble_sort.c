f218fdeceebebf126855689a6ad5f66afd1d7fa5
share-bingo <73519040+share-bingo@users.noreply.github.com>
2021-01-10 00:30:13 +0800

    Create bubble_sort.c
    创建了冒泡排序的c语言程序

---
+++ bubble_sort.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void bubble_sort(int *arr, int len);
+
+int main(void){
+
+  int arr[10] = {2, 3, 7, 6, 1, 4, 8, 5, 10, 9};
+  bubble_sort(arr, 10);
+
+  return 0;
+}
+
+
+void bubble_sort(int *arr, int len){
+
+
+}
